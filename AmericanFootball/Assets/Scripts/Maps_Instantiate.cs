6f5fcb88f729cfe34615b6c84f1002be372fd76d
Lhtbay <baseralper.yalniz@yandex.ru>
2022-05-12 15:56:09 +0300

    12.05.22
    SecondSave

---
+++ AmericanFootball/Assets/Scripts/Maps_Instantiate.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maps_Instantiate : MonoBehaviour
+{
+    [Header ("Maps Settings")]
+    [SerializeField] private GameObject _mapObject;
+    [SerializeField] private GameObject _mapWinObject;
+    [SerializeField] private int _mapMaxCount;
+
+    public List<GameObject> _mapsList;
+    private int _lastListCount= 0;
+
+    private void Start()
+    {
+        for (int i = 0; i < _mapMaxCount; i++)
+        {           
+            if (_mapsList.Count == 0)
+            {               
+                _mapsList = new List<GameObject>();
+                GameObject newMap = Instantiate(_mapObject, transform.position, Quaternion.identity);
+
+                newMap.transform.parent = transform;
+
+                _mapsList.Add(newMap);                             
+            }
+            else
+            {
+                GameObject newMap = Instantiate(_mapObject, 
+                    _mapsList[_lastListCount].transform.position+new Vector3 (0,0,+10), 
+                    Quaternion.identity);
+
+                newMap.transform.parent = transform;
+             
+                _mapsList.Add(newMap);
+                _lastListCount++;
+          
+            }
+        }
+        GameObject winnerMap = Instantiate(_mapWinObject,
+                    _mapsList[_lastListCount].transform.position + new Vector3(0, 0, +10.2f),
+                    Quaternion.identity);
+
+        winnerMap.transform.parent = transform;
+
+        _mapsList.Add(winnerMap);
+        _lastListCount++;
+    }
+
+}
---
+++ AmericanFootball/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [Header("Player Settings")]
+    [SerializeField] private float _playerNormalSpeed;
+    [SerializeField] private float _playerRuningSpeed;
+    [SerializeField] private float _normalXLocation;
+    [SerializeField] private float _runningXLocation;
+    [SerializeField] private float _leftPassTime;
+    [SerializeField] private float _anglePassTime;
+    [SerializeField] private float _returnNormalizedTime;
+    private Transform _normalZRotation;
+    private Transform _runningZRotation;
+    private Transform _backNormalRotation;
+    private Transform _playerTransform;
+    private Animator _playerAnim;
+
+    [Header("Camera Settings")]
+    [SerializeField] private int _normalFOV;
+    [SerializeField] private int _runingsFOV;
+    [SerializeField] private float _smoothTime;
+    private Camera _mainCamera;
+
+    private float _playerSpeed;
+    private float _newXPosition;
+    private float _timer;
+
+    private int _randomWinAnimationCount;
+
+    private bool _stop = false;
+
+    private void Start()
+    {
+        _mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        _playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        _playerAnim = _playerTransform.GetComponent<Animator>();
+
+        _normalZRotation = GameObject.FindGameObjectWithTag("NorRot").gameObject.transform;
+        _runningZRotation = GameObject.FindGameObjectWithTag("RunRot").gameObject.transform;
+        _backNormalRotation = GameObject.FindGameObjectWithTag("BackRot").gameObject.transform;
+
+        _timer = _returnNormalizedTime;
+    }
+
+    private void Update()
+    {
+        if (!_stop)
+        {
+            PlayerController();
+
+            transform.Translate(Vector3.forward * Time.deltaTime * _playerSpeed);
+        }
+        else
+        {                    
+            _playerAnim.SetTrigger("win"+_randomWinAnimationCount);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Winner")
+        {
+            _stop = true;
+            _randomWinAnimationCount = Random.Range(1, 4);
+        }
+        else if (other.gameObject.tag == "Enemy")
+        {
+
+        }
+    }   
+
+    #region METODS
+
+    private void PlayerController()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _timer = 0;
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            _timer = 0;
+        }
+        if (Input.GetButton("Fire1"))
+        {
+            _playerSpeed = _playerRuningSpeed;
+
+            _newXPosition = Mathf.Lerp(transform.position.x, _runningXLocation, _leftPassTime*Time.deltaTime);
+            
+            transform.position = new Vector3(
+                _newXPosition,
+                transform.position.y,
+                transform.position.z);
+
+           
+            _timer += Time.deltaTime;
+
+            if (_timer >= _returnNormalizedTime)
+            {
+                _playerTransform.rotation = Quaternion.Lerp(
+                _playerTransform.rotation,
+                _normalZRotation.rotation,
+                _anglePassTime * Time.deltaTime);
+            }
+            else
+            {
+                _playerTransform.rotation = Quaternion.Lerp(
+                _playerTransform.rotation,
+                _runningZRotation.rotation,
+                _anglePassTime * Time.deltaTime);
+            }
+
+            _mainCamera.fieldOfView = Mathf.Lerp(_mainCamera.fieldOfView, _runingsFOV, _smoothTime* Time.deltaTime);
+
+        }
+        else
+        {
+            _playerSpeed = _playerNormalSpeed;
+
+            _newXPosition = Mathf.Lerp(transform.position.x, _normalXLocation, _leftPassTime * Time.deltaTime);
+
+            transform.position = new Vector3(
+               _newXPosition,
+               transform.position.y,
+               transform.position.z);
+           
+            _timer += Time.deltaTime;
+
+            if (_timer >= _returnNormalizedTime)
+            {
+                _playerTransform.rotation = Quaternion.Lerp(
+                _playerTransform.rotation,
+                _normalZRotation.rotation,
+                _anglePassTime * Time.deltaTime);
+            }
+            else
+            {
+                _playerTransform.rotation = Quaternion.Lerp(
+                _playerTransform.rotation,
+                _backNormalRotation.rotation,
+                _anglePassTime * Time.deltaTime);
+            }
+
+            _mainCamera.fieldOfView = Mathf.Lerp(_mainCamera.fieldOfView, _normalFOV, _smoothTime * Time.deltaTime);
+        }
+    }
+
+    #endregion
+
+}
